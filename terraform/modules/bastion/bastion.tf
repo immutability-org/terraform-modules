02385dcaa5ba34384cb4dae9db7423c8cf70b1d1
Jeff Ploughman <jeff_ploughman@troweprice.com>
2016-12-16 16:35:11 -0500

    Bastion

--- terraform/modules/bastion/bastion.tf
+++ terraform/modules/bastion/bastion.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "bastion" {
     subnet_id     = "${var.subnet_id}"
 
     vpc_security_group_ids      = ["${aws_security_group.bastion.id}"]
-    associate_public_ip_address = false
+    associate_public_ip_address = true
 
     tags {
         Name = "${var.tag_name}_${count.index}"
